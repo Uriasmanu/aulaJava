f1efe9e34d1e0b184bb439adf49a745e9509cc23
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-12-05 12:05:53 -0300

    .

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int nota = 0;
+
+        for(int i = 0; i<3; i++){
+            System.out.println(i);
+            nota =+ nota + 1;
+        }
+
+        System.out.println(nota/2);
     }
 }
